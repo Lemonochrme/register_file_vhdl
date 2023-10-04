c19d995b283e9a7381593644ba67978021a02132
Robin <contact@lemonochrome.fr>
2023-10-04 14:55:22 +0200

    Register file design done, need to make a proper testbench

--- register_file/register_file.srcs/sim_1/new/testbench.vhd
+++ register_file/register_file.srcs/sim_1/new/testbench.vhd
@@ -1,43 +1,76 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 28.09.2023 16:04:26
--- Design Name: 
--- Module Name: testbench - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_ARITH.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
+ENTITY register_file_tb IS
+END register_file_tb;
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+ARCHITECTURE tb_arch OF register_file_tb IS
+    SIGNAL Q_A, Q_B: std_logic_vector(7 DOWNTO 0);
+    SIGNAL READ_ADRESS_A, READ_ADRESS_B, W_ADRESS: std_logic_vector(3 DOWNTO 0);
+    SIGNAL W_ENABLE, RST, CLK: std_logic;
+    SIGNAL DATA_IN: std_logic_vector(7 DOWNTO 0) := (OTHERS => '0');
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+    COMPONENT register_file
+        PORT (
+            Q_A, Q_B : OUT std_logic_vector(7 DOWNTO 0);
+            READ_ADRESS_A, READ_ADRESS_B : IN std_logic_vector(3 DOWNTO 0);
+            W_ADRESS : IN std_logic_vector(3 DOWNTO 0) := (OTHERS => '0');
+            W_ENABLE : IN std_logic;
+            DATA_IN : IN std_logic_vector(7 DOWNTO 0);
+            RST : IN std_logic;
+            CLK : IN std_logic
+        );
+    END COMPONENT;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+BEGIN
+    UUT: register_file
+        PORT MAP (
+            Q_A => Q_A,
+            Q_B => Q_B,
+            READ_ADRESS_A => READ_ADRESS_A,
+            READ_ADRESS_B => READ_ADRESS_B,
+            W_ADRESS => W_ADRESS,
+            W_ENABLE => W_ENABLE,
+            DATA_IN => DATA_IN,
+            RST => RST,
+            CLK => CLK
+        );
 
-entity testbench is
---  Port ( );
-end testbench;
+    PROCESS
+    BEGIN
+        -- Initialize signals
+        READ_ADRESS_A <= "0000";
+        READ_ADRESS_B <= "0001";
+        W_ADRESS <= "0000";
+        W_ENABLE <= '0';
+        DATA_IN <= "00000000";
+        RST <= '0';
+        CLK <= '0';
+        WAIT FOR 10 ns; 
 
-architecture Behavioral of testbench is
+        
+        W_ENABLE <= '1';
+        DATA_IN <= "01010101";
+        W_ADRESS <= "0000";
+        WAIT FOR 40 ns;
+        W_ENABLE <= '0';
+                
+        W_ENABLE <= '1';
+        DATA_IN <= "11111111";
+        W_ADRESS <= "0001";
+        WAIT FOR 40 ns;
+        W_ENABLE <= '0';
+        
+        RST <= '0';
 
-begin
+        -- Clock Oscillation
+        LOOP
+            WAIT FOR 5 ns;  -- Half period
+            CLK <= NOT CLK; -- Toggle the clock
+        END LOOP;
 
-
-end Behavioral;
+        WAIT;
+    END PROCESS;
+END tb_arch;
--- register_file/register_file.srcs/sources_1/new/design.vhd
+++ register_file/register_file.srcs/sources_1/new/design.vhd
@@ -1,7 +1,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
-use IEEE.NUMERIC_STD.ALL;
 
 
 
@@ -14,59 +14,42 @@ use IEEE.NUMERIC_STD.ALL;
 --        CLK           -|>           |
 --                       +------------+
 
---        READ_ADRESS_A : Registres 0 à 7
---        READ_ADRESS_B : Registres de 8 à 15
 
--- READ_ADRESS_A      Array Index
---         000 :      0
---         001 :      1
---         010 :      2
---         011 :      3
---         100 :      4
---         101 :      5
---         110 :      6
---         111 :      7
+ENTITY register_file IS
+	PORT (
+		Q_A, Q_B : OUT std_logic_vector(7 DOWNTO 0);
+		READ_ADRESS_A, READ_ADRESS_B : IN std_logic_vector(3 DOWNTO 0); -- Read Pointer
+		W_ADRESS : IN std_logic_vector(3 DOWNTO 0) := (OTHERS => '0'); -- Write Pointer
+		W_ENABLE : IN std_logic;
+		DATA_IN : IN std_logic_vector(7 DOWNTO 0); -- Data to write
+		RST : IN std_logic;
+		CLK : IN std_logic
+		);
+	END register_file; -- Lecture asynchrone on sort toujours les donnees en fonction de des read addr (Combinatoire)
 
--- READ_ADRESS_B      Array Index
---         000 :      8
---         001 :      9
---         010 :      10
---         011 :      11
---         100 :      12
---         101 :      13
---         110 :      14
---         111 :      15
+	ARCHITECTURE register_file_arch OF register_file IS
+		TYPE REG_ARRAY IS ARRAY (0 TO 15) OF std_logic_vector(7 DOWNTO 0);
+		SIGNAL Registers : REG_ARRAY := (OTHERS => "00000000");
+	BEGIN
+		Q_A <= Registers(CONV_INTEGER(unsigned(READ_ADRESS_A))) WHEN (W_Enable = '0' OR READ_ADRESS_A /= W_ADRESS)
+		       ELSE
+		       DATA_IN;
+ 
+		Q_B <= Registers(CONV_INTEGER(unsigned(READ_ADRESS_B))) WHEN (W_Enable = '0' OR READ_ADRESS_B /= W_ADRESS)
+		       ELSE DATA_IN;
 
-
-
-entity register_file is
-    Port(
-        Q_A, Q_B : out std_logic_vector(7 downto 0);
-        READ_ADRESS_A, READ_ADRESS_B : in std_logic_vector(3 downto 0); -- Read Pointer
-        W_ADRESS : in std_logic_vector(3 downto 0) := (others => '0'); -- Write Pointer
-        W_ENABLE : in std_logic;
-        DATA_IN : in std_logic_vector(7 downto 0); -- Data to write
-        RST : in std_logic;
-        CLK : in std_logic
-        );
-end register_file; -- Lecture asynchrone on sort toujours les donnees en fonction de des read addr (Combinatoire)
-
-architecture register_file_arch of register_file is
-        type REG_ARRAY is array (0 to 15) of std_logic_vector(7 downto 0);
-        signal Registers : REG_ARRAY := (others => "00000000");
-        signal Data_A, Data_B : std_logic_vector(7 downto 0);
-begin
-
-    if W_ENABLE = '1' AND CLK = '1' then
-        
-    else
-    end if;
-
-
-    Data_A <= Registers(to_integer(unsigned(READ_ADRESS_A)));
-    Data_B <= Registers(to_integer(unsigned(READ_ADRESS_B)));
-    Q_A <= Data_A;
-    Q_B <= Data_B;
-
-
-end register_file_arch;
+		-- Write data synchronously
+		PROCESS (W_ADRESS, W_ENABLE, DATA_IN, RST, CLK) IS
+		BEGIN
+			IF RST = '0' THEN
+				Registers <= (OTHERS => "00000000");
+			END IF;
+			IF RST = '1' THEN
+				IF CLK'EVENT AND CLK = '1' THEN
+					IF W_Enable = '1' THEN
+						Registers(CONV_INTEGER(unsigned(W_ADRESS))) <= DATA_IN;
+					END IF;
+				END IF;
+			END IF;
+		END PROCESS;
+END register_file_arch;
